b43f2535cba2e42e9f5834eb58484169d3a463ed
Stefano Galati <s.galati@jetop.com>
2024-05-03 00:08:25 +0200

    GreenButton typescript fix

--- src/components/atoms/GreenButton.tsx
+++ src/components/atoms/GreenButton.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import { ReactNode } from 'react';
 
 
 
 
 
-const GreenButton = ({ children }) => {
+const GreenButton = (props : {children: ReactNode}) => {
    return (
       <button className="bg-[#009379] h-[60px] py-[16px] px-[50px] text-white flex justify-center rounded-2xl font-[600] text-[16px] items-center">
-      {children}
+         {props.children}
       </button>
    );
  };
