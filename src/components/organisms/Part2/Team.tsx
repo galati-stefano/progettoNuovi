d83cf2d5139b4143af292f8c99582e4491361acc
feliantonio <francesco.eliantonio@gmail.com>
2024-05-02 21:24:37 +0200

    ultima modifica su altezza pagine francesco

--- src/components/organisms/Part2/Team.tsx
+++ src/components/organisms/Part2/Team.tsx
@@ -5,7 +5,7 @@ import Card1 from "../../molecole/Card1"
 const Team = () => {
     return (
 
-        <div className="flex flex-col lg:h-screen gap-y-16 bg-[#F8F9FF] py-[60px] px-[30px] lg:py-[120px] lg:[55px]"> 
+        <div className="flex flex-col gap-y-16 bg-[#F8F9FF] py-[60px] px-[30px] lg:py-[120px] lg:[55px]"> 
             <Title title="Meet our team" subtitle="Get to know the faces behind the scenes and learn about the values that drive us."/>
             <div className="flex justify-center flex-wrap gap-10">
                 <Card1 imgPath="src\assets\sarah.svg" name="Sarah K." role="Lead Designer" description="With a passion for color and a love for clean lines, Sarah brings all our wildest design dreams to life." />
--- src/components/organisms/Part2/Testimonials.tsx
+++ src/components/organisms/Part2/Testimonials.tsx
@@ -7,7 +7,7 @@ const Testimonials = () => {
     let desc1: string = "\"Anima’s Landing Page UI Kit has become a staple in my design toolkit. This kit has everything I need to get the job done quickly and efficiently.\"";
     let desc2: string = "\"The Landing Page UI Kit has been a game changer. The pre-designed components and templates have helped us deliver projects faster!\"";
     return (
-        <div className="flex flex-col lg:h-screen gap-y-16 lg:gap-y-32 bg-[#F8D57E] py-[100px] px-[32px] lg:py-[120px] lg:[55px]">
+        <div className="flex flex-col gap-y-16 lg:gap-y-32 bg-[#F8D57E] py-[100px] px-[32px] lg:py-[120px] lg:[55px]">
             <Title title = "Real Stories from Satisfied Customers" subtitle = "See how our landing page ui kit is making an impact"/>
             <div className="flex flex-wrap gap-8 justify-center">
                 <Card imgPath="src\assets\lauren.svg" name="Lauren M." role="UI Designer @Boo" description={desc1} />
