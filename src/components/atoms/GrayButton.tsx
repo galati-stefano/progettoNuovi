c4437905f4fc1a1b899892c2e635288466333b67
Stefano Galati <s.galati@jetop.com>
2024-05-03 13:31:35 +0200

    GrayButton typescript fix

--- src/components/atoms/GrayButton.tsx
+++ src/components/atoms/GrayButton.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
+import { ReactNode } from 'react';
 
 
 
-
-
-const GrayButton = ({ children },{classNamea}) => {
+const GrayButton = (props : {children: ReactNode, classNamea?: string}) => {
    return (
-      <button className={`bg-[#E5F4F2] h-[60px] py-[16px] px-[50px] text-[#009379] flex justify-center rounded-2xl font-[600] text-[16px] items-center ${classNamea}`}>
-      {children}
+      <button className={`bg-[#E5F4F2] h-[60px] py-[16px] px-[50px] text-[#009379] flex justify-center rounded-2xl font-[600] text-[16px] items-center ${props.classNamea}`}>
+      {props.children}
       </button>
    );
  };
